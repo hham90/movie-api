5fc91c18b465c1e083a7c62cd5d9241da9e90031
hham90 <148152147+hham90@users.noreply.github.com>
2023-11-28 12:48:37 -0500

    added auth and passport
    added passport
    added auth code

---
+++ auth.js
@@ -0,0 +1,36 @@
+const jwtSecret = 'your_jwt_secret'; // has to be same key used in JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+    passport = require ('passport');
+
+require('./passport'); // your local passport file
+
+let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.Username, // Username is what you're encoding
+        expiresIn: '7d', // How long token will last
+        algorithm: 'HS256' // algorithm used to encode the values of JWT
+    });
+}
+
+/* POST login. */
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false }, (error,
+    user, info) => {
+        if (error || !user) {
+            return res.status(400).json({
+                message: 'Something is not right',
+                user: user
+            });
+        }
+        req.login(user, { session: false }, (error) => {
+            if (error) {
+                res.send(error);
+            }
+            let token = generateJWTToken(user.toJSON());
+            return res.json({ user, token });
+        });
+      })(req, res);
+    });
+}
