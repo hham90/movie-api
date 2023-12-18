4c46a5af72cdf4231c910bf9902a541940482dd2
hham90 <148152147+hham90@users.noreply.github.com>
2023-12-18 09:53:25 -0500

    reenabled auth
    for /movies endpoint

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to my Top Movies');
 });
 
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
